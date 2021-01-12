842ec3f2892e540aedd9de48134b961a31a1dad9
Martin <argentoar@hotmail.com.ar>
2021-01-11 21:29:32 -0300

    actualizados los archivos desde mi pc

--- js/script.js
+++ js/script.js
@@ -1,99 +1,112 @@
+// VARIABLES
+
 const boxArray = Array.from(document.querySelectorAll(".box"));
-const [boxOne, boxTwo, boxThree, boxFour, boxFive, boxSix, boxSeven, boxEight, boxNine] = boxArray;
 const scoreCounter = document.querySelector(".counter").children[1];
 const firstTurn = document.querySelector(".turn").children[1];
 const [choicex, choiceo] = Array.from(document.querySelectorAll(".btn"));
 const computerArray = [];
 const startButton = document.querySelector(".start");
 
-let userFigure = "x";
-let computerFigure = "o";
+let userFigure = "X";
+let computerFigure = "O";
 let score = 0;
 let winner;
 let userName = "Nombre";
 let turn;
-let gameStatus = "playing";
+let gameStatus = "gameover";
 let userPlays = [];
 let computerPlays = [];
+
+// FUNCIONES
+
+/*Eleccion de la figura del usuario*/
+// function choiceFigure () {
+//     if (gameStatus !== "playing") {
+// choicex.addEventListener("click", () => {
+//     choicex.classList.add("select");
+//     choiceo.classList.remove("select");
+//     return userFigure = "X", computerFigure = "O";
+// });
+
+// choiceo.addEventListener("click", () => {
+//     choiceo.classList.add("select");
+//     choicex.classList.remove("select");
+//     return userFigure = "O", computerFigure= "X";
+// });
+//     }
+// };
+
+// choiceFigure();
+
+/* Boton "Comenzar a jugar" */
+startButton.addEventListener('click', () => {
+    if (gameStatus === "gameover") {
+        startButton.textContent = "Jugando...";
+        firstTurnAssign();
+        return gameStatus = "playing";
+    };
+})
 
 /*sorteo del primer turno*/
 function firstTurnAssign() {
     let randomNumber = Math.floor(Math.random()*10);
     if (randomNumber >= 5) {
         firstTurn.textContent = `${userName}`;
-        return turn = "player";
+        userPrint();
+        // return turn = "player";        
     } else {
         firstTurn.textContent = "Computer";
-        return turn = "computer";}
+        computerPrintDelay(computerPrint);
+        // return turn = "computer";
+    }
 }
 
-firstTurnAssign();
-
-/* Boton "Comenzar a jugar" */
-// startButton.addEventListener('click', () => {
-//     if (gameStatus === "gameover") {
-//         startButton.textContent = "Jugando...";
-//         return gameStatus === "playing";
-//     } else return;
-// })
-
-/*Eleccion de la figura del usuario*/
-function choiceFigure () {
-    if (gameStatus !== "playing") {
-choicex.addEventListener("click", () => {
-    choicex.classList.add("select");
-    choiceo.classList.remove("select");
-    return userFigure = "X", computerFigure = "O";
-});
-
-choiceo.addEventListener("click", () => {
-    choiceo.classList.add("select");
-    choicex.classList.remove("select");
-    return userFigure = "O", computerFigure= "X";
-});
-    }
-};
-
-choiceFigure();
 
 /*cambio de turno*/
-function nextTurn () {
-    if (turn === "computer") {
-        firstTurn.textContent = `${userName}`;
-        return turn = "player";
-    } else {firstTurn.textContent = `Computer`;
-        return turn = "computer";
-    };
-}
+// function nextTurn () {
+//     if (turn === "computer") {
+//         firstTurn.textContent = `${userName}`;
+//         return turn = "player";
+//     } else {firstTurn.textContent = `Computer`;
+//         return turn = "computer";
+//     };
+// }
 
 /*imprime figura del usuario*/
 function userPrint () {
 boxArray.forEach(box => {
     box.addEventListener('click', () => {
-        if(box.dataset.status !== "taken" && turn === "player"){
+        if(box.dataset.status !== "taken"){
         box.textContent = `${userFigure}`;
-        box.dataset.status = "taken";
-        box.dataset.who = "byUser";
         userPlays.push(boxArray.indexOf(box));
         winnerChecker(userPlays);
-        nextTurn();
-    } else return ;
+        // nextTurn();
+        if(gameStatus !== "gameover" ){
+            computerPrintDelay(computerPrint);
+        }
+    };
     })
 });
 }
 
 /* imprime figura de la computadora */
+function computerPrintDelay(print) {
+    setTimeout(print, 1000);
+}
+
 function computerPrint () {
     let i = Math.floor(Math.random()*9);
     let box = boxArray[i];
-    if (box.dataset.status !== "taken" && turn === "computer"){
+    if (box.dataset.status !== "taken"){
     box.textContent= `${computerFigure}`;
-    box.dataset.status = "taken";
-    box.dataset.who = "byComputer";
+    box.dataset.status = "taken";;
     computerPlays.push(boxArray.indexOf(box));
     winnerChecker(computerPlays);
-    nextTurn();
-    } else return;
+    // nextTurn();
+    if(gameStatus !== "gameover" ){
+        userPrint();
+    }
+    };
 }
 
 /* verifica si los numeros se encuentran dentro del array */
@@ -120,18 +133,3 @@ function winnerChecker(arraytocheck) {
     }
 }
 
-// analizar bien
-function startPlaying () {
-    for (let i = 0; i <= 8; i++) {
-        if (gameStatus === "playing") {
-            if (turn === "player"){
-                userPrint();
-              }
-              else if (turn === "computer"){
-                computerPrint();   
-              }
-        } else return;
-    }
-};
-
-startPlaying();
